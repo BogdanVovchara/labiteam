485c0d83796a31a16ae0ab2e6fa3eff1f762fb3d
Bohdan <you@example.com>
2018-08-14 08:21:31 +0300

    small edits

--- js/handler.js
+++ js/handler.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-	const username = document.querySelector('rss-form input[type="text"]').value,
-	submit = document.querySelector('rss-form input[type="submit"]');
-	
-    https://www.pinterest.com/{username}/feed.rss/
-});
+	const username = document.querySelector('.rss-form input[type="text"]'),
+	    submit = document.querySelector('.rss-form input[type="submit"]'),
+	    url = `https://www.pinterest.com/${username.value}/feed.rss/`,
+	    xhr = new XMLHttpRequest();
+	submit.addEventListener('submit', callName);
+
+	function callName() {
+	    xhr.open('GET', 'url', true);
+	    xhr.send();
+	};
